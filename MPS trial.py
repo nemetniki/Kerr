c12c6f4b8faca4a32dad2f4cc9e1e1537f8efd81
nemetniki <nnem614@aucklanduni.ac.nz>
2018-02-07 14:11:04 +1300

    .py added

---
+++ MPS trial.py
@@ -0,0 +1,222 @@
+import numpy as np
+import scipy as sc
+import matplotlib.pyplot as plt
+from scipy.linalg import svd
+import matplotlib as mpl
+mpl.rcParams['mathtext.fontset'] = 'cm'
+mpl.rcParams['mathtext.rm'] = 'serif'
+mpl.rc('font',family='FreeSerif')
+mpl.rc('xtick',labelsize=30)
+mpl.rc('ytick',labelsize=30)
+
+### Linestyles ###
+colors={'red':(241/255.,88/255.,84/255.),        'orange':(250/255,164/255.,58/255.),        'pink':(241/255,124/255.,176/255.),        'brown':(178/255,145/255.,47/255.),        'purple':(178/255,118/255.,178/255.),        'green':(96/255,189/255.,104/255.),        'blue':(93/255,165/255.,218/255.),        'yellow':(222/255., 207/255., 63/255),        'black':(0.,0.,0.)}
+collab = ['brown','green','blue','pink','black']
+linewidth = [2,2,3,3,4]
+linestyle = ['solid','dashed','dashdot','dotted','solid']
+
+
+# ## 1. Hamiltonian in the rotating frame
+# 
+# The system Hamiltonian:
+# 
+# \begin{align}
+# \hat{H}_S=\hbar\Delta_c\hat{c}^\dagger\hat{c} + \hbar\Delta_e\hat{\sigma}_+\hat{\sigma}_-
+#             +\hbar g\left(\hat{c}^\dagger\hat{\sigma}_-+\hat{\sigma}_+\hat{c}\right)
+#             +\frac{\hbar\Omega_c}{2}\left(\hat{c}+\hat{c}^\dagger\right)
+#             +\frac{\hbar\Omega_e}{2}\left(\hat{\sigma}_-+\hat{\sigma}_+\right)
+# \end{align}
+# 
+# The interaction with the environment with feedback:
+# 
+# \begin{align}
+# \hat{H}_{fb}(t)=-i\hbar\left\{\left[\sqrt{\gamma_R}\hat{b}(t-\tau)e^{-i\phi}+\sqrt{\gamma_L}\hat{b}(t)\right]\hat{c}^\dagger-\left[\sqrt{\gamma_R}\hat{b}^\dagger(t-\tau)e^{i\phi}+\sqrt{\gamma_L}\hat{b}^\dagger(t)\right]\hat{c}\right\}
+# \end{align}
+# 
+# where the feedback phase is
+# \begin{align}
+# \phi=\pi-\omega_L\tau
+# \end{align}
+# 
+# The bath is originally Markovain with
+# \begin{align}
+# \left[\hat{b}(t),\hat{b}^\dagger(t^\prime)\right]=\delta(t-t^\prime)
+# \end{align}
+# 
+
+# ## 2. Time-evolution
+# 
+# \begin{align}
+# |\Psi(t_{k+1})\rangle=U(t_{k+1},t_k)|\Psi(t_k)\rangle
+# \end{align}
+# 
+# with $\Psi$ written as a Matrix Product State. The time-evolution operator can be expanded as
+# \begin{align}
+# U(t_{k+1},t_k) &= \exp{\left[-\frac{i}{\hbar}\left(H_S\Delta t+\int_{t_k}^{t_{k+1}}H_{fb}(t)dt\right)\right]}\\
+# {\bf U}&=\exp{\left({\bf M}_S+{\bf M}_B\right)} = \sum_{n=0}^\infty\frac{1}{n!}\left({\bf M}_S+{\bf M}_B\right)^n
+# \end{align}
+# where $t_k=k\Delta t$ and $\Delta B(t_k) = \int_{t_k}^{t_{k+1}}b(t)dt$. This means that
+# \begin{align}
+# \left[\Delta B(t_k),\Delta B^\dagger(t_j)\right] = \Delta t \delta_{k,j}
+# \end{align}
+# 
+# Therefore the different orders of the expansion above are:
+# \begin{align}
+# {\bf U} &=\mathbb{1}+\color{red}{{\bf M}_B}+\color{orange}{{\bf M}_S+\frac{1}{2}{\bf M}_B^2}+
+#             \color{green}{\frac{1}{2}\left({\bf M}_S{\bf M}_B+{\bf M}_B{\bf M}_S\right)+\frac{1}{6}{\bf M}_B^3}+
+#             \color{blue}{\frac{1}{2}{\bf M}_S^2+\frac{1}{6}\left({\bf M}_S{\bf M}_B^2+{\bf M}_B{\bf M}_S{\bf M}_B+{\bf M}_B^2{\bf M}_S\right)+\frac{1}{24}{\bf M}_B^4}+\mathcal{O}(\Delta t^{5/2})
+# \end{align}
+# This means that to first order we have:
+# <img src='U_mat_JCM.png'>
+
+# ## 3. Test case #1: no feedback
+# Let us consider a TLS on a waveguide. We need up to 4 photons in the environment and a system vector with size 2. $\gamma_R=0$
+# <img src='U_mat_TLS.png'>
+
+# In[8]:
+
+
+#index in the init list = timebin index: 0=16, ..., 15=1, 16=0, 17=S, 18=-1, 19=-2, 20=-3
+endt=10000
+init = [0]*endt
+l=3
+initTLS = np.array([0,1]) #starting at |e>
+
+gamma_L = 0.
+Om_TLS  = np.pi#*gamma_L
+Delta_T = 0.0
+dt      = .0001
+# Initial state of the system
+init[endt-4] = initTLS
+
+### This function basically implements the formula above without feedback ###
+def U(tk,tS): #tk: time bin state at k, tS: state of S
+    U_m      = sc.eye(tk.size,tk.size,1)*np.sqrt(np.arange(0,tk.size)) # decreasing the number of photons in the environment bin with 1
+    #print(np.dot(U_m,tk))
+    U_p      = sc.eye(tk.size,tk.size,-1)*np.sqrt(np.arange(1,tk.size+1)) # increasing the number of photons in the environment bin with 1
+    #print(np.dot(U_p,tk))
+    sm       = sc.eye(2,2,1) # sigma_-
+    sp       = sc.eye(2,2,-1) # sigma_+
+    
+    ### Different terms in U ###
+    #####First step####
+    T_0      = np.tensordot(tk,tS,0) #identity operation
+    T_sqdt_1 = -np.sqrt(dt)*np.tensordot(np.dot(U_m,tk),np.dot(sp,tS),0)*np.sqrt(gamma_L)
+    #print(T_sqdt_1)
+    T_sqdt_2 = np.sqrt(dt)*np.tensordot(np.dot(U_p,tk),np.dot(sm,tS),0)*np.sqrt(gamma_L)
+    #print(T_sqdt_2)
+    T_dt_S_1 = -1j*dt*Delta_T*np.tensordot(tk,np.dot(np.array([[0,0],[0,1]]),tS),0)
+    #print(T_dt_S_1)
+    T_dt_S_2 = -1j*dt*Om_TLS*np.tensordot(tk,np.dot(sp,tS),0)
+    #print(T_dt_S_2)
+    T_dt_S_3 = -1j*dt*Om_TLS*np.tensordot(tk,np.dot(sm,tS),0)
+    #print(T_dt_S_3)
+    T_dt_B_1 = -.5*gamma_L*dt*np.tensordot(np.arange(1,tk.size+1)*tk,np.dot(np.array([[0,0],[0,1]]),tS),0)
+    #print(T_dt_B_1)
+    T_dt_B_2 = -.5*gamma_L*dt*np.tensordot(np.arange(0,tk.size)*tk,np.dot(np.array([[1,0],[0,0]]),tS),0)
+    #print(T_dt_B_2)
+        
+    nextstep = T_0 + T_sqdt_1 + T_sqdt_2 + T_dt_S_1 + T_dt_S_2 + T_dt_S_3 + T_dt_B_1 + T_dt_B_2
+    return(nextstep)
+
+print("initial excitation:",np.einsum("k,k",np.einsum("i,ik",initTLS,np.array([[0,0],[0,1]])),np.conjugate(initTLS)))
+print("initial ground probability:",np.einsum("k,k",np.einsum("i,ik",initTLS,np.array([[1,0],[0,0]])),np.conjugate(initTLS)))
+m=0
+exc2 = np.zeros(endt-3,complex)
+norm = np.zeros(endt-3,complex)
+exc = np.zeros(endt-3,complex)
+for m in np.arange(endt-4,-1,-1):
+#    print("before\n",init[m].shape)
+#    print(init[m])
+#    if m<endt-4:
+#        print(init[m+1].shape)
+#    print(m)
+
+    ### Calculating the norm ###
+    if m==endt-4:
+        nor1 = np.dot(init[m],np.conjugate(init[m]))
+    elif m==endt-5:
+        nor2 = np.einsum("ik,jk",init[m+1],np.conjugate(init[m+1]))
+        #print(nor2.shape)
+        nor1 = np.einsum("ij,ij",np.einsum("li,lj",init[m],np.conjugate(init[m])),nor2)
+    else:
+        nor2 = np.einsum("minj,ij",np.einsum("mli,nlj",init[m+1],np.conjugate(init[m+1])),nor2)
+        #print(nor2.shape)
+        nor1 = np.einsum("mn,mn",np.einsum("lm,ln",init[m],np.conjugate(init[m])),nor2)
+    norm[m] = nor1
+
+    ### Determining the excitation level of the qubit ###
+    if m==endt-4:
+        exc[m]=np.einsum("k,k",np.einsum("i,ik",init[m],np.array([[1,0],[0,0]])),np.conjugate(init[m]))
+        exc2[m]=np.einsum("k,k",np.einsum("i,ik",init[m],np.array([[0,0],[0,1]])),np.conjugate(init[m]))
+    else:
+        exc2[m]=np.einsum("jk,kj",np.einsum("ij,ik",init[m],np.array([[0,0],[0,1]])),np.conjugate(init[m]))
+        exc[m]=np.einsum("jk,kj",np.einsum("ij,ik",init[m],np.array([[1,0],[0,0]])),np.conjugate(init[m]))
+    
+    ### A time-step ###
+    # We consider up to 4 excitations in the environmental timebin (first array)
+    together = U(np.array([1,0,0,0,0]),init[m])
+#    print("together shape\n",together.shape)
+    if m==endt-4:
+        tog_swap = np.einsum("ij->ji",together)
+        tog_svd  = svd(tog_swap,full_matrices=False)
+    else:
+        tog_swap = np.einsum("ijk->jik",together)
+        di = tog_swap[0,:,0].size
+        dj = tog_swap[:,0,0].size
+        dk = tog_swap[0,0,:].size
+#        print("di=",di,", dk=",dk)
+        tog_swap_conc = np.zeros((dj,di*dk),dtype=np.complex128)
+        for i in range(0,tog_swap[0,:,0].size):
+            for k in range(0,tog_swap[0,0,:].size):
+                tog_swap_conc[:,i+di*k]=tog_swap[:,i,k]
+        tog_svd_conc = svd(tog_swap_conc,full_matrices=False)
+        tog_svd = [0.+0*1j]*3
+        tog_svd[2] = np.zeros((dj,di,dk),dtype=np.complex128)
+        for I in range(0,tog_swap_conc[:,0].size):
+            tog_svd[2][:,I%di,int((I-(I%di))/di)]  = tog_svd_conc[0][:,I]
+        tog_svd[1] = tog_svd_conc[1]
+        tog_svd[0] = tog_svd_conc[0]
+
+    ### Storing the different Matrices of the timebins and system (I'm not convinced that this is necessary) ###
+    init[m] = tog_svd[2]
+    init[m-1] = np.dot(tog_svd[0],np.diag(tog_svd[1]))
+#    print("after\n",init[m].shape)
+#    print(init[m-1].shape)
+#print(init[-1])
+
+### Plotting to be happy ###
+t=np.arange(0,endt-3)*dt*1#gamma_L
+plt.figure(figsize = (12,7))
+plt.plot(t,np.flip(np.real(exc),0),lw=2,label="ground",color=colors["green"])
+plt.plot(t,np.flip(np.real(exc2),0),lw=2,label="excited",color=colors["blue"])
+#plt.plot(t,np.exp(-gamma_L*t),ls="--",lw=4,color=colors["orange"],label="$\exp{(-\gamma_Lt)}$")
+#plt.xlim(0,8)
+#plt.ylim(-0.01,1.01)
+plt.xlabel("$\gamma_Lt$",fontsize=40)
+plt.ylabel("$\left<\sigma_+\sigma_-\\right>$",fontsize=40)
+plt.legend(fontsize=25,loc="upper right")
+plt.grid(True)
+plt.figure(2,figsize = (12,7))
+plt.plot(t,np.flip(np.real(norm-1),0),lw=2,label="norm",color=colors["purple"])
+plt.grid(True)
+plt.xlim(-.1,1.1)
+
+
+# In[6]:
+
+
+plt.figure(figsize = (12,7))
+plt.plot(t,np.flip(np.real(exc),0),lw=2,label="ground",color=colors["green"])
+plt.plot(t,np.flip(np.real(exc2),0),lw=2,label="excited",color=colors["blue"])
+plt.plot(t,np.flip(np.real(exc),0)+np.flip(np.real(exc2),0),lw=2,label="ground",color=colors["yellow"])
+#plt.plot(t,np.exp(-2*gamma_L*t),ls="--",lw=4,color=colors["orange"],label="$\exp{(-\gamma_Lt)}$")
+#plt.xlim(0,8)
+#print(np.flip(np.real(exc),0)+np.flip(np.real(exc2),0))
+#print(np.flip(np.real(exc),0))
+#print(np.flip(np.real(exc2),0))
+#plt.ylim(-0.01,1.01)
+plt.xlabel("$t$",fontsize=40)
+plt.ylabel("$\left<\sigma_+\sigma_-\\right>$",fontsize=40)
+plt.grid(True)
+
