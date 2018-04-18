b0f626205261f9249ce1ec72eb2c9d6903aeed34
nemetniki <nnem614@aucklanduni.ac.nz>
2018-04-18 18:10:23 +1200

    Spectrum really working now. couple of test runs done

--- MPS_JC+fb_sparse.py
+++ MPS_JC+fb_sparse.py
@@ -146,86 +146,6 @@ def SVD_sig(block,cutoff):
 #\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
 #/////////////////////////////////////////////////////////////////////////////////////////////////////////////#
 
-############################
-### Calculating the norm ###
-############################
-def normf(M,L,state,norm_L):
-	"""Performing the contractions for the norm
-	INPUT: calculated states in list "state" with a delay index-length L 
-	and the stored values of norm_L (tensor with indices for state and dual) at timestep M
-	OUTPUT: the norm at the present time and the contraction of the past bins which stay constant"""
-
-	# Indices of the timebins initially: 0->furthest past, L->system, L+1->first future timebin
-	if M==0:
-		norm = np.einsum("i,i",state[L],np.conjugate(state[L]))
-	else:
-		#print(state[M-1].shape,norm_L.shape)
-	# Contracting part of the MPS that won't change anymore with its dual and with previously stored tensors
-		if len(state[M-1].shape)==1:
-			norm_L = np.einsum("i,i",state[M-1],np.conjugate(state[M-1]))
-		elif len(state[M-1].shape)==2:
-			norm_L = np.einsum("ik,jk->ij",state[M-1],np.conjugate(state[M-1]))
-		else:
-			norm_L = np.einsum("kmj,lmj->kl",np.einsum("kmi,ij->kmj",state[M-1],norm_L),np.conjugate(state[M-1]))
-        
-		# Contracting the system part of the MPS
-		if len(state[L+M].shape)==1:
-			norm_S = np.dot(state[L+M],np.conjugate(state[L+M]))
-		else:
-			norm_S = np.einsum("ki,kj->ij",state[L+M],np.conjugate(state[L+M]))
-		norm = norm_L
-
-	# Performing the rest of the reservoir contractions from right to left.
-		for i in range(0,L):
-			#print("state",state[M+i].shape)
-			if len(state[M+i].shape)==1:
-				norm_past = np.einsum("i,i",state[M+i],np.conjugate(state[M+i]))
-				norm = norm_past*norm
-				#print("norm",norm.shape)
-			elif len(state[M+i].shape)==2:
-				norm_past = np.einsum("ij,kj->ik",state[M+i],np.conjugate(state[M+i]))
-				if len(norm.shape)>0:
-					if norm_past.shape[0]!=norm.shape[0]:
-						norm_past = np.einsum("ii", norm_past)
-				norm = norm_past*norm
-#				print("norm",norm.shape)
-			else:
-				norm = np.einsum("kmj,lmj->kl",np.einsum("kmi,ij->kmj",state[M+i],norm),np.conjugate(state[M+i]))
-				#print("norm",norm.shape)
-	# Contracting the environment part with the system part
-		if len(state[L+M].shape) ==1:
-			if len(norm.shape) <=1:
-				norm = norm*norm_S
-			else:
-				norm = np.einsum("ii",norm)*norm_S
-		else:
-			norm = np.einsum("ij,ij",norm,norm_S)
-		norm_S = None
-	return np.real(norm),norm_L
-
-#/////////////////////////////////////////////////////////////////////////////////////////////////////////////#
-#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
-#/////////////////////////////////////////////////////////////////////////////////////////////////////////////#
-
-##############################################
-### Expectation value of system observable ###
-##############################################
-def exp_sys(observable,sys,M):
-	"""Calculating the expectation value of a given system observable
-	INPUT: observable of interest, the state of the system and timestep M
-	OUTPUT: expectation value of the observable"""
-
-	# Indices of the timebins initially: 0->furthest past, L->system, L+1->first future timebin
-	if len(sys.shape)==1:
-		obs = np.einsum("i,i",np.einsum("i,ij",sys,observable),np.conjugate(sys))
-	else:
-		obs = np.einsum("jk,kj",np.einsum("ij,ik->jk",sys,observable),np.conjugate(sys))
-	return np.real(obs)
-
-#/////////////////////////////////////////////////////////////////////////////////////////////////////////////#
-#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
-#/////////////////////////////////////////////////////////////////////////////////////////////////////////////#
-
 #######################
 ### Merging indices ###
 #######################
@@ -551,8 +471,9 @@ def SWAP(states,base_ind,direction,OC=None):
 				# the cut function puts the OC to the right by default. In order to move the interacting past
 				# bin next to the system bin, the orthogonality centre should also shift towards the left (future).
 				if direction =="future":
-					states[base_ind+c*s+1],states[base_ind+c*s] = OC_reloc(states[base_ind+c*s+1],
-                                                                           states[base_ind+c*s],"left",tol)
+					if len(states[base_ind+c*s].shape)>1:
+						states[base_ind+c*s+1],states[base_ind+c*s] = OC_reloc(states[base_ind+c*s+1],
+                                                                               states[base_ind+c*s],"left",tol)
 			else:
 				print("problem with tensor rank in swap")
                 
@@ -808,6 +729,173 @@ def U(tk,tS,tl,M): #tk: time bin state at k, tS: state of S
 
     return initial + sys + env + sys_env##
 
+#*************************#
+#***-------------------***#
+#***| ANALYZING TOOLS |***#
+#***-------------------***#
+#*************************#
+
+############################
+### Calculating the norm ###
+############################
+def normf(M,L,state,norm_L):
+	"""Performing the contractions for the norm
+	INPUT: calculated states in list "state" with a delay index-length L 
+	and the stored values of norm_L (tensor with indices for state and dual) at timestep M
+	OUTPUT: the norm at the present time and the contraction of the past bins which stay constant"""
+
+	# Indices of the timebins initially: 0->furthest past, L->system, L+1->first future timebin
+	if M==0:
+		norm = np.einsum("i,i",state[L],np.conjugate(state[L]))
+	else:
+		#print(state[M-1].shape,norm_L.shape)
+	# Contracting part of the MPS that won't change anymore with its dual and with previously stored tensors
+		if len(state[M-1].shape)==1:
+			norm_L = np.einsum("i,i",state[M-1],np.conjugate(state[M-1]))
+		elif len(state[M-1].shape)==2:
+			norm_L = np.einsum("ik,jk->ij",state[M-1],np.conjugate(state[M-1]))
+		else:
+			norm_L = np.einsum("kmj,lmj->kl",np.einsum("kmi,ij->kmj",state[M-1],norm_L),np.conjugate(state[M-1]))
+        
+		# Contracting the system part of the MPS
+		if len(state[L+M].shape)==1:
+			norm_S = np.dot(state[L+M],np.conjugate(state[L+M]))
+		else:
+			norm_S = np.einsum("ki,kj->ij",state[L+M],np.conjugate(state[L+M]))
+		norm = norm_L
+
+	# Performing the rest of the reservoir contractions from right to left.
+		for i in range(0,L):
+			#print("state",state[M+i].shape)
+			if len(state[M+i].shape)==1:
+				norm_past = np.einsum("i,i",state[M+i],np.conjugate(state[M+i]))
+				norm = norm_past*norm
+				#print("norm",norm.shape)
+			elif len(state[M+i].shape)==2:
+				norm_past = np.einsum("ij,kj->ik",state[M+i],np.conjugate(state[M+i]))
+				if len(norm.shape)>0:
+					if norm_past.shape[0]!=norm.shape[0]:
+						norm_past = np.einsum("ii", norm_past)
+				norm = norm_past*norm
+#				print("norm",norm.shape)
+			else:
+				norm = np.einsum("kmj,lmj->kl",np.einsum("kmi,ij->kmj",state[M+i],norm),np.conjugate(state[M+i]))
+				#print("norm",norm.shape)
+	# Contracting the environment part with the system part
+		if len(state[L+M].shape) ==1:
+			if len(norm.shape) <=1:
+				norm = norm*norm_S
+			else:
+				norm = np.einsum("ii",norm)*norm_S
+		else:
+			norm = np.einsum("ij,ij",norm,norm_S)
+		norm_S = None
+	return np.real(norm),norm_L
+
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////#
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////#
+
+##############################################
+### Expectation value of system observable ###
+##############################################
+def exp_sys(observable,sys,M):
+	"""Calculating the expectation value of a given system observable
+	INPUT: observable of interest, the state of the system and timestep M
+	OUTPUT: expectation value of the observable"""
+
+	# Indices of the timebins initially: 0->furthest past, L->system, L+1->first future timebin
+	if len(sys.shape)==1:
+		obs = np.einsum("i,i",np.einsum("i,ij",sys,observable),np.conjugate(sys))
+	else:
+		obs = np.einsum("jk,kj",np.einsum("ij,ik->jk",sys,observable),np.conjugate(sys))
+	return np.real(obs)
+
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////#
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////#
+
+#######################
+### Output spectrum ###
+#######################
+def spectrum(states,freqs,pmax,N_env):
+    """Calculating the expectation value of a given system observable
+    INPUT: observable of interest, the state of the system and timestep M
+    OUTPUT: expectation value of the observable"""
+    index = N-L-1
+    dB  = sc.eye(N_env,N_env,1)*np.sqrt(dt*np.arange(0,N_env)) 
+    dBd = sc.eye(N_env,N_env,-1)*np.sqrt(dt*np.arange(1,N_env+1)) 
+    sum_B     = np.zeros(freqs.shape,complex)
+    if len(states[index].shape)==1:
+        sum_B      = np.einsum("l,l",np.einsum("j,jl->l",states[index],np.einsum("jf,fl->jl",dBd,dB)),
+                               np.conjugate(states[index]))*np.ones(freqs.shape)
+        next_step  = np.einsum("l,l",np.einsum("j,jl->l",states[index],dBd),np.conjugate(states[index]))
+    elif len(states[index].shape)==2:
+        ind = states[index].shape.index(np.max(states[index].shape))
+        if ind==0:
+            sum_B      = np.einsum("lk,lk",np.einsum("jk,jl->lk",states[index],np.einsum("jf,fl->jl",dBd,dB)),
+                                   np.conjugate(states[index]))*np.ones(freqs.shape)
+            next_step  = np.einsum("lk,lm->km",np.einsum("jk,jl->lk",states[index],dBd),np.conjugate(states[index]))
+        elif ind==1:
+            sum_B      = np.einsum("kl,kl",np.einsum("kj,jl->kl",states[index],np.einsum("jf,fl->jl",dBd,dB)),
+                                   np.conjugate(states[index]))*np.ones(freqs.shape)
+            next_step  = np.einsum("kl,kl",np.einsum("kj,jl->kl",states[index],dBd),np.conjugate(states[index]))
+    elif len(states[index].shape)==3:
+        sum_B      = np.einsum("ilk,ilk",np.einsum("jl,ijk->ilk",np.einsum("jf,fl->jl",dBd,dB),states[index]),
+                               np.conjugate(states[index]))*np.ones(freqs.shape)
+        next_step  = np.einsum("ilk,ilm->km",np.einsum("ijk,jl->ilk",states[index],dBd),np.conjugate(states[index]))
+    for p in range(1,pmax):
+        if len(states[index-p].shape)==1:
+            if np.isscalar(next_step):
+                sum_B     += (np.einsum("l,l",np.einsum("m,ml->l",states[index-p],dB),
+                                        np.conjugate(states[index-p]))*next_step*np.exp(1j*freqs*p*dt))
+                next_step  = next_step*np.einsum("j,j",states[index-p],np.conjugate(states[index-p]))
+            else:
+                sum_B     += (np.einsum("ii",next_step)*
+                              np.einsum("l,l",np.einsum("m,ml->l",states[index-p],dB),
+                                        np.conjugate(states[index-p]))*np.exp(1j*freqs*p*dt))
+                next_step  = np.einsum("ii",next_step)*np.einsum("m,m",states[index-p],np.conjugate(states[index-p]))
+        if len(states[index-p].shape)==2:
+            indp = states[index-p].shape.index(np.max(states[index-p].shape))
+            if indp == 0:
+                if np.isscalar(next_step):
+                    sum_B     += (next_step*np.einsum("lk,lk",np.einsum("mk,ml->lk",states[index-p],dB),
+                                                      np.conjugate(states[index-p]))*np.exp(1j*freqs*p*dt))
+                    next_step  = next_step*np.einsum("mk,ml->kl",states[index-p],np.conjugate(states[index-p]))
+                else:
+                    sum_B     += (np.einsum("ii",next_step)*
+                                  np.einsum("lk,lk",np.einsum("mk,ml->lk",states[index-p],dB),
+                                            np.conjugate(states[index-p]))*np.exp(1j*freqs*p*dt))
+                    next_step  = np.einsum("ii",next_step)*np.einsum("mk,ml->kl",states[index-p],
+                                                                 np.conjugate(states[index-p]))
+            elif indp == 1:
+                if np.isscalar(next_step):
+                    sum_B     += (np.einsum("ii",np.einsum("il,jl->ij",np.einsum("im,ml->il",states[index-p],dB),
+                                                           np.conjugate(states[index-p])))*
+                                  next_step*np.exp(1j*freqs*p*dt))
+                    next_step  = next_step*np.einsum("ij,ij",states[index-p],np.conjugate(states[index-p]))
+                else:
+                    sum_B     += (np.einsum("ij,ij",np.einsum("il,jl->ij",np.einsum("im,ml->il",states[index-p],dB),
+                                                              np.conjugate(states[index-p])),next_step)*
+                                  np.exp(1j*freqs*p*dt))
+                    next_step  = np.einsum("kj,jk",np.einsum("ij,ik->kj",next_step,states[index-p]),
+                                           np.conjugate(states[index-p]))
+        elif len(states[index-p].shape)==3:
+            if np.isscalar(next_step):
+                sum_B     += (np.einsum("ilk,ilk",np.einsum("imk,ml->ilk",states[index-p],dB),
+                                        np.conjugate(states[index-p]))*next_step*np.exp(1j*freqs*p*dt))
+                next_step  = next_step*np.einsum("imk,imk",states[index-p],np.conjugate(states[index-p]))
+            else:
+                sum_B     += (np.einsum("ij,ij",np.einsum("ilk,jlk->ij",np.einsum("imk,ml->ilk",states[index-p],dB),
+                                                          np.conjugate(states[index-p])),next_step)*
+                              np.exp(1j*freqs*p*dt))
+                next_step  = np.einsum("jmk,jml->kl",np.einsum("ij,imk->jmk",next_step,states[index-p]),
+                                       np.conjugate(states[index-p]))
+    return 2./dt*np.real(sum_B)
+    
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////#
+#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\#
+#/////////////////////////////////////////////////////////////////////////////////////////////////////////////#
 
 # In[ ]:
 
@@ -952,9 +1040,10 @@ for M in range(0,N-L-1):
         file_out.close()
 
 
-    # Erase the remnants of the states that will not influence the dynamics anymore:
-    if M>0:
-        states[M-1] = None
+#    # Erase the remnants of the states that will not influence the dynamics anymore:
+#    if M>0:
+#        states[M-1] = None
+#   It is needed for the spectral calculations
 
     # The time evolution operator acting on the interacting state bins
 #    print("initial shapes",initenv.shape, states[ind_sys].shape, states[ind_sys-1].shape)
@@ -998,6 +1087,15 @@ for M in range(0,N-L-1):
 # and relocating the orthogonality centre
 if len(states[M].shape)>1:
     states[M+1],states[M] = OC_reloc(states[M+1],states[M],"left",tol)
+
+#Calculating the output spectra over a range of frequencies
+om = np.linspace(-20,20,5000)
+spec = spectrum(states,om,N-L-1,N_env+1)
+if args.plot==False:
+	time_out = np.transpose(np.vstack((om,spec)))
+	np.savetxt("./Data/spec_JC+fb_gL=%dp10_gR=%dp10_g=%dp10_phi=%dp10pi_initind=%d.txt" % (gamma_L*10, gamma_R*10, g*10, args.phi*10,args.init_ind),time_out)
+	time_out=None
+
 states[M+1:M+L+1] = SWAP(states,M+1,"future","left")
 if len(states[ind_sys-1].shape)>1:
     states[ind_sys],states[ind_sys-1] = OC_reloc(states[ind_sys],states[ind_sys-1],"left",tol)
@@ -1050,6 +1148,13 @@ if args.plot:
 	plt.xlabel("$t$",fontsize=40)
 	plt.grid(True)
 	plt.ylabel("Cavity photon number",fontsize=40)
+	plt.figure(figsize = (12,7))
+	plt.plot(om,spec,lw=3,color=colors["blue"],ls="-")
+	plt.xlabel("$\omega$",fontsize=40)
+	plt.grid(True)
+	plt.legend(loc="best",fontsize = 25)
+	plt.ylabel("Spectrum",fontsize=40)
+
 #	plt.ylim(-.05,2.)
 	plt.show()
 
