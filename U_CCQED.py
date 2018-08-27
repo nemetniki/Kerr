878676c156b361a7356ac44fc25051f447471bef
nemetniki <nnem614@aucklanduni.ac.nz>
2018-08-27 13:31:08 +1200

    writing U

--- U_CCQED.py
+++ U_CCQED.py
@@ -5,11 +5,12 @@ import time
 import sys
 from decimal import Decimal
 from math import factorial
+from opt_einsum import contract
 
 ############################
 ### Evolution operator U ###
 ############################
-def U(M,tF1,tF2,tS1,tB1,tB2,tS2,gamma_B1,gamma_B2,gamma_F1,gamma_F2,dt,phi,Ome1,Ome2,Omc1,Omc2,g1,g2,Delc1,Delc2,Dele): #tk: time bin state at k, tS: state of S1
+def U(M,tF1,tF2,tS1,tB1,tB2,tS2,gamma_B,gamma_F,dt,phi,Ome,Omc,g,Delc,Dele): #tk: time bin state at k, tS: state of S1
 	"""Evolution operator up to dt^2
 	INPUT: states at the current time (t_k), the delayed time (t_l) and the system state (t_S) at timestep M
 	Remember, at M=0 the state is separable
@@ -32,36 +33,6 @@ def U(M,tF1,tF2,tS1,tB1,tB2,tS2,gamma_B1,gamma_B2,gamma_F1,gamma_F2,dt,phi,Ome1,
 #    print("dims",dim_tk,dim_tS,dim_tl)
 
     #####Frequently used operators#####
-	def B(state):
-		new_state_tk = np.zeros(state.shape,complex)
-		new_state_tl = np.zeros(state.shape,complex)
-		n = np.arange(1,state.shape[0])
-		if legs==3:
-			if gamma_L is not 0:
-				new_state_tk[0:-1,:,:] = np.einsum("i,ijk->ijk",np.sqrt(n*gamma_L*dt),state[1:,:,:])
-			if gamma_R is not 0:
-				new_state_tl[:,:,0:-1] = np.einsum("k,ijk->ijk",np.sqrt(n*gamma_R*dt)*np.exp(-1j*phi),state[:,:,1:])
-		elif legs==4:
-			if gamma_L is not 0:
-				new_state_tk[0:-1,:,:,:] = np.einsum("i,ijkl->ijkl",np.sqrt(n*gamma_L*dt),state[1:,:,:,:])
-			if gamma_R is not 0:
-				new_state_tl[:,:,0:-1,:] = np.einsum("k,ijkl->ijkl",np.sqrt(n*gamma_R*dt)*np.exp(-1j*phi),state[:,:,1:,:])
-		return new_state_tk+new_state_tl
-	def Bd(state):
-		new_state_tk = np.zeros(state.shape,complex)
-		new_state_tl = np.zeros(state.shape,complex)
-		n = np.arange(1,state.shape[0])
-		if legs==3:
-			if gamma_L is not 0:
-				new_state_tk[1:,:,:] = np.einsum("i,ijk->ijk",np.sqrt(n*gamma_L*dt),state[0:-1,:,:])
-			if gamma_R is not 0:
-				new_state_tl[:,:,1:] = np.einsum("k,ijk->ijk",np.sqrt(n*gamma_R*dt)*np.exp(-1j*phi),state[:,:,0:-1])
-		elif legs==4:
-			if gamma_L is not 0:
-				new_state_tk[1:,:,:,:] = np.einsum("i,ijkl->ijkl",np.sqrt(n*gamma_L*dt),state[0:-1,:,:,:])
-			if gamma_R is not 0:
-				new_state_tl[:,:,1:,:] = np.einsum("k,ijkl->ijkl",np.sqrt(n*gamma_R*dt)*np.exp(-1j*phi),state[:,:,0:-1,:])
-		return new_state_tk+new_state_tl
 	def a(state,which):
 		new_state = np.zeros(state.shape,complex)
 		n=np.linspace(1,int((dim_tS)/2),dim_tS-2).astype(np.int64)
@@ -120,82 +91,146 @@ def U(M,tF1,tF2,tS1,tB1,tB2,tS2,gamma_B1,gamma_B2,gamma_F1,gamma_F2,dt,phi,Ome1,
 		new_tS_g = np.zeros(state.shape,complex)
 		new_tS_Ome = np.zeros(state.shape,complex)
 		n=np.linspace(1,int((dim_tS)/2),dim_tS-2).astype(np.int64)
-		if which == 1:
-			if M == 0:
-				if g1 is not 0:
+		if g[which-1] is not 0:
+			if which == 1:
+				if M == 0:
 					new_tS_g[:,:,1:dim_tS-1:2,:,:,:]   = np.einsum("j,iljkmn->iljkmn",np.sqrt(n[0:dim_tS-1:2])*g1,state[:,:,2:dim_tS:2,:,:,:])
 					new_tS_g[:,:,2:dim_tS:2,:,:,:]   = np.einsum("j,iljkmn->iljkmn",np.sqrt(n[0:dim_tS-1:2])*g1,state[:,:,1:dim_tS-1:2,:,:,:])
-				if Ome1 is not 0:
-					new_tS_Ome[:,:,0:dim_tS-2:2,:,:,:] = Ome1*state[:,:,1:dim_tS-1:2,:,:,:]
-					new_tS_Ome[:,:,1:dim_tS-1:2,:,:,:] = Ome1*state[:,:,0:dim_tS-2:2,:,:,:]
-			else:
-				if g1 is not 0:
+				else:
 					new_tS_g[:,:,1:dim_tS-1:2,:,:,:,:,:]   = np.einsum("j,imjklnop->imjklnop",np.sqrt(n[0:dim_tS-1:2])*g1,state[:,:,2:dim_tS:2,:,:,:,:,:])
 					new_tS_g[:,:,2:dim_tS:2,:,:,:,:,:]   = np.einsum("j,imjklnop->imjklnop",np.sqrt(n[0:dim_tS-1:2])*g1,state[:,:,1:dim_tS-1:2,:,:,:,:,:])
-				if Ome1 is not 0:
-					new_tS_Ome[:,:,0:dim_tS-2:2,:,:,:,:,:] = Ome1*state[:,:,1:dim_tS-1:2,:,:,:,:,:]
-					new_tS_Ome[:,:,1:dim_tS-1:2,:,:,:,:,:] = Ome1*state[:,:,0:dim_tS-2:2,:,:,:,:,:]
-		elif which == 2:
-			if M == 0:
-				if g2 is not 0:
+			elif which == 2:
+				if M == 0:
 					new_tS_g[:,:,:,1:dim_tS-1:2,:,:]   = np.einsum("j,ilkjmn->ilkjmn",np.sqrt(n[0:dim_tS-1:2])*g2,state[:,:,:,2:dim_tS:2,:,:])
 					new_tS_g[:,:,:,2:dim_tS:2,:,:]   = np.einsum("j,ilkjmn->ilkjmn",np.sqrt(n[0:dim_tS-1:2])*g2,state[:,:,:,1:dim_tS-1:2,:,:])
-				if Ome2 is not 0:
-					new_tS_Ome[:,:,:,0:dim_tS-2:2,:,:] = Ome2*state[:,:,:,1:dim_tS-1:2,:,:]
-					new_tS_Ome[:,:,:,1:dim_tS-1:2,:,:] = Ome2*state[:,:,:,0:dim_tS-2:2,:,:]
-			else:
-				if g2 is not 0:
+				else:
 					new_tS_g[:,:,:,:,1:dim_tS-1:2,:,:,:]   = np.einsum("j,imkljnop->imkljnop",np.sqrt(n[0:dim_tS-1:2])*g2,state[:,:,:,:,2:dim_tS:2,:,:,:])
 					new_tS_g[:,:,:,:,2:dim_tS:2,:,:,:]   = np.einsum("j,imkljnop->imkljnop",np.sqrt(n[0:dim_tS-1:2])*g2,state[:,:,:,:,1:dim_tS-1:2,:,:,:])
-				if Ome2 is not 0:
+		if Ome[which-1] is not 0:
+			if which == 1:
+				if M == 0:
+					new_tS_Ome[:,:,0:dim_tS-2:2,:,:,:] = Ome1*state[:,:,1:dim_tS-1:2,:,:,:]
+					new_tS_Ome[:,:,1:dim_tS-1:2,:,:,:] = Ome1*state[:,:,0:dim_tS-2:2,:,:,:]
+				else:
+					new_tS_Ome[:,:,0:dim_tS-2:2,:,:,:,:,:] = Ome1*state[:,:,1:dim_tS-1:2,:,:,:,:,:]
+					new_tS_Ome[:,:,1:dim_tS-1:2,:,:,:,:,:] = Ome1*state[:,:,0:dim_tS-2:2,:,:,:,:,:]
+			elif which == 2:
+				if M == 0:
+					new_tS_Ome[:,:,:,0:dim_tS-2:2,:,:] = Ome2*state[:,:,:,1:dim_tS-1:2,:,:]
+					new_tS_Ome[:,:,:,1:dim_tS-1:2,:,:] = Ome2*state[:,:,:,0:dim_tS-2:2,:,:]
+				else:
 					new_tS_Ome[:,:,:,:,0:dim_tS-2:2,:,:,:] = Ome2*state[:,:,:,:,1:dim_tS-1:2,:,:,:]
 					new_tS_Ome[:,:,:,:,1:dim_tS-1:2,:,:,:] = Ome2*state[:,:,:,:,0:dim_tS-2:2,:,:,:]
 		return new_tS_g+new_tS_Ome
-	def nc(state):
+	def nc(state,which):
 		n=np.linspace(0,int((dim_tS)/2),dim_tS).astype(np.int64)
 		new_state = np.zeros(state.shape,complex)
-		if len(state.shape)==3:
-			new_state = np.einsum("j,ijk->ijk",n,state)
-		elif len(state.shape)==4:
-			new_state = np.einsum("j,ijkl->ijkl",n,state)
+		if which == 1:
+			if M==0:
+				new_state = np.einsum("j,imjkno->imjkno",n,state)
+			else:
+				new_state = np.einsum("j,imjklnop->imjklnop",n,state)
+		elif which == 2:
+			if M==0:
+				new_state = np.einsum("j,imkjno->imkjno",n,state)
+			else:
+				new_state = np.einsum("j,imkljnop->imkljnop",n,state)
 		return new_state
-	def C(state):
-		if Delc is 0 and Omc is 0:
-			return 0.
-		elif Delc is 0 and Omc is not 0:
-			return Omc*(c(state)+cd(state))
-		elif Delc is not 0 and Omc is 0:
-			return Delc*nc(state)
+	def C(state,which):
+		if Delc[which-1] is 0 and Omc[which-1] is 0:
+			return 0.*state
+		elif Delc[which-1] is 0 and Omc[which-1] is not 0:
+			return Omc[which-1]*(c(state,which)+cd(state,which))
+		elif Delc[which-1] is not 0 and Omc[which-1] is 0:
+			return Delc[which-1]*nc(state,which)
 		else:
-			return Delc*nc(state)+Omc*(c(state)+cd(state))
-	def MB(state):
-		return Bd(c(state))-B(cd(state))
-	def MS(state):
+			return Delc[which-1]*nc(state,which)+Omc[which-1]*(c(state,which)+cd(state,which))
+	def MS(state,which):
 		new_tS = np.zeros(state.shape,complex)
 		new_tS += state
-		if Dele is 0:
-			return -1j*dt*(C(state)+JC(state))
+		if Dele[which-1] is 0:
+			return -1j*dt*(C(state,which)+JC(state,which))
 		else:
-			if legs==3:
-				new_tS[:,0:dim_tS:2,:] = 0
-			elif legs==4:
-				new_tS[:,0:dim_tS:2,:,:] = 0
-			return -1j*dt*(C(state)+JC(state)+Dele*new_tS)
-	#don't worry about this one!
-	def Nn(tk,tS,tl,NBmin,NBmax):
-		B_tk,B_tl = B(tk,tl)
-		BdB_tk,BdB_tl = Bd(B_tk,B_tl)
-		B_tk = None
-		B_tl = None
-		nc = np.linspace(0,(dim_tS-1)/2+.1,dim_tS).astype(np.int64)
-		state = 0
-		if legs==3:
-			for i in range(NBmin,NBmax+1):
-				state = np.tensordot(BdB_tk+(gamma_L+gamma_R)*dt*tk*i, np.tensordot((nc+1-i)*tS,BdB_tl,0),0)+state
-		elif legs==4:
-			for i in range(NBmin,NBmax+1):
-				state = np.tensordot(BdB_tk+(gamma_L+gamma_R)*dt*tk*i, np.einsum("ij,jkl",(nc+1-i)*tS,BdB_tl))+state
-		return state
+			if which == 1:
+				if M==0:
+					new_tS[:,:,0:dim_tS:2,:,:,:] = 0
+				else:
+					new_tS[:,:,0:dim_tS:2,:,:,:,:,:] = 0
+			elif which == 1:
+				if M==0:
+					new_tS[:,:,:,0:dim_tS:2,:,:] = 0
+				else:
+					new_tS[:,:,:,:,0:dim_tS:2,:,:,:] = 0
+			return -1j*dt*(C(state,which)+JC(state,which)+Dele[which-1]*new_tS)
+	def E(state,which,N):
+		new_state_tB = np.zeros(state.shape,complex)
+		new_state_tF = np.zeros(state.shape,complex)
+		n = np.arange(N,state.shape[0])
+		nprod = 1.
+		i = 0
+		while i<N:
+			nprod *= n-i
+			i+=1
+		if gamma_B[which-1] != 0.:
+			if which==1:
+				if M==0:
+					new_state_tB[:,0:-N,:,:,:,:] = np.einsum("i,mijkln->mijkln",np.sqrt(nprod*gamma_B[which-1]*dt),state[:,N:,:,:,:,:])
+				else:
+					new_state_tB[:,0:-N,:,:,:,:,:,:] = np.einsum("i,mijklnop->mijklnop",np.sqrt(nprod*gamma_B[which-1]*dt),state[:,N:,:,:,:,:,:,:])
+			elif which==2:
+				if M==0:
+					new_state_tB[:,:,:,:,0:-N,:] = np.einsum("i,mjklin->mjklin",np.sqrt(nprod*gamma_B[which-1]*dt),state[:,:,:,:,N:,:])
+				else:
+					new_state_tB[:,:,:,:,:,:,0:-N,:] = np.einsum("i,mjklnoip->mjklnoip",np.sqrt(nprod*gamma_B[which-1]*dt),state[:,:,:,:,:,:,N:,:])
+			if gamma_F[which-1] != 0:
+			if which==1:
+				if M==0:
+					new_state_tF[0:-N,:,:,:,:,:] = np.einsum("i,ijklmn->ijklmn",np.sqrt(nprod*gamma_F[which-1]*dt),state[N:,:,:,:,:,:])*np.exp(-1j*phi)
+				else:
+					new_state_tF[0:-N,:,:,:,:,:,:,:] = np.einsum("i,ijklmnop->ijklmnop",np.sqrt(nprod*gamma_F[which-1]*dt),state[N:,:,:,:,:,:,:,:])*np.exp(-1j*phi)
+			elif which==2:
+				if M==0:
+					new_state_tF[:,:,:,:,:,0:-N] = np.einsum("i,mjklni->mjklni",np.sqrt(nprod*gamma_F[which-1]*dt),state[:,:,:,:,:,N:])*np.exp(-1j*phi)
+				else:
+					new_state_tF[:,:,:,:,:,:,:,0:-N] = np.einsum("i,mjklnopi->mjklnopi",np.sqrt(nprod*gamma_F[which-1]*dt),state[:,:,:,:,:,:,:,N:])*np.exp(-1j*phi)
+		return new_state_tB+new_state_tF
+	def Ed(state,which,N):
+		new_state_tB = np.zeros(state.shape,complex)
+		new_state_tF = np.zeros(state.shape,complex)
+		n = np.arange(N,state.shape[0])
+		nprod = 1.
+		i = 0
+		while i<N:
+			nprod *= n+i
+			i+1
+		if gamma_B[which-1] != 0.:
+			if which==1:
+				if M==0:
+					new_state_tB[:,N:,:,:,:,:] = np.einsum("i,mijkln->mijkln",np.sqrt(nprod*gamma_B[which-1]*dt),state[:,0:-N,:,:,:,:])
+				else:
+					new_state_tB[:,N:,:,:,:,:,:,:] = np.einsum("i,mijklnop->mijklnop",np.sqrt(nprod*gamma_B[which-1]*dt),state[:,0:-N,:,:,:,:,:,:])
+			elif which==2:
+				if M==0:
+					new_state_tB[:,:,:,:,N:,:] = np.einsum("i,mjklin->mjklin",np.sqrt(nprod*gamma_B[which-1]*dt),state[:,:,:,:,0:-N,:])
+				else:
+					new_state_tB[:,:,:,:,:,:,N:,:] = np.einsum("i,mjklnoip->mjklnoip",np.sqrt(nprod*gamma_B[which-1]*dt),state[:,:,:,:,:,:,0:-N,:])
+			if gamma_F[which-1] != 0:
+			if which==1:
+				if M==0:
+					new_state_tF[N:,:,:,:,:,:] = np.einsum("i,ijklmn->ijklmn",np.sqrt(nprod*gamma_F[which-1]*dt),state[0:-N,:,:,:,:,:])*np.exp(-1j*phi)
+				else:
+					new_state_tF[N:,:,:,:,:,:,:,:] = np.einsum("i,ijklmnop->ijklmnop",np.sqrt(nprod*gamma_F[which-1]*dt),state[0:-N,:,:,:,:,:,:,:])*np.exp(-1j*phi)
+			elif which==2:
+				if M==0:
+					new_state_tF[:,:,:,:,:,N:] = np.einsum("i,mjklni->mjklni",np.sqrt(nprod*gamma_F[which-1]*dt),state[:,:,:,:,:,0:-N])*np.exp(-1j*phi)
+				else:
+					new_state_tF[:,:,:,:,:,:,:,N:] = np.einsum("i,mjklnopi->mjklnopi",np.sqrt(nprod*gamma_F[which-1]*dt),state[:,:,:,:,:,:,:,0:-N])*np.exp(-1j*phi)
+		return new_state_tB+new_state_tF
+	def nE(state,which):
+	
+	def ME(state,which):
+		return ad(E(state,which,1),which)-a(Ed(state,which,1),which)
+	
         
     ####----------------------####
     #### Different terms in U ####
@@ -203,9 +238,9 @@ def U(M,tF1,tF2,tS1,tB1,tB2,tS2,gamma_B1,gamma_B2,gamma_F1,gamma_F2,dt,phi,Ome1,
     
     #####Initial state#####
 	if M==0:
-		initial = np.tensordot(tF1,tB1,tS1,tS2,tB2,tF2)
+		initial = contract("i,j,k,l,m,n->ijklmn",tF1,tB1,tS1,tS2,tB2,tF2)
 	else:
-		initial = np.einsum("oip,j,okqr,pltu,m,unr->ijkqltmn",tFL,tBL,tS1,tS2,tB2,tF2)
+		initial = contract("oip,j,okqr,pltu,m,unr->ijkqltmn",tFL,tBL,tS1,tS2,tB2,tF2)
 #    print("init",initial[0,2,0],initial[1,0,0],initial[0,0,1],initial[0,0,0], initial.shape)
 	legs = len(initial.shape)
 	
